3a25e1fd36d20756df6fbfcc2559766afa53f37f
p1512972 <devnull@localhost>
2015-10-13 16:22:25 +0200

    merge

--- REST/src/main/java/fr/univlyon1/chat/ConversationsServlet.java
+++ REST/src/main/java/fr/univlyon1/chat/ConversationsServlet.java
@@ -7,13 +7,10 @@ package fr.univlyon1.chat;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.regex.Pattern;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
